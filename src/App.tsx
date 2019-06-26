677e9c82c5cdfb1a97141c927358246fee676982
Vegar Eeg <vegareeg@gmail.com>
2019-06-26 21:16:40 +0200

    initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import clsx from 'clsx';
 
 const App: React.FC = () => {
+  const [selected, setSelected] = React.useState('');
+
+  const handleClick = (box: string) => {
+    setSelected(box);
+  };
+
+  const listStyles = {
+    justifyContent: ''
+  };
+
+  const formDetails = <div className="details">details</div>;
+
+  const box1 = (
+    <li key="box1" onClick={() => handleClick('box1')}>
+      box1
+      {formDetails}
+    </li>
+  );
+
+  const box2 = (
+    <li key="box2" onClick={() => handleClick('box2')}>
+      box2
+      {formDetails}
+    </li>
+  );
+
+  const box3 = (
+    <li key="box3" onClick={() => handleClick('box3')}>
+      box3
+      {formDetails}
+    </li>
+  );
+
+  let items = [box1, box2, box3];
+
+  if (selected === 'box1') {
+    items = [box1];
+    listStyles.justifyContent = 'flex-start';
+  } else if (selected === 'box2') {
+    items = [box2];
+    listStyles.justifyContent = 'center';
+  } else if (selected === 'box3') {
+    items = [box3];
+    listStyles.justifyContent = 'flex-end';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div onClick={() => handleClick('')}>clear</div>
+      <ul
+        className={clsx('list', selected && 'is-selected')}
+        style={listStyles}
+      >
+        {items}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
